ad066a3588ee87592e52910437eff92ed10f404c
Dmitry Yunkov <dima.yonkov@gmail.com>
2018-03-09 22:40:40 +0300

    addd

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 gulp.task('default', function () {
  gulp.src('./sass/**/*.scss')
    .pipe(sass().on('error', sass.logError))
-   .pipe(gulp.dest('./page/resources/css'));
+   .pipe(gulp.dest('./page/assets/css'));
 });
 
  gulp.watch('./sass/**/*.scss', ['default']);
